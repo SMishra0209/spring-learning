23e8e12542dd7efaa1c20ccb2513606721c4b958
Shivesh Mishra <shiveshmishra020906@gmail.com>
2026-09-11 10:52:26 +0530

    Stereotype annotation implementation

--- src/main/java/springCore/chapter2/ex5/Dog.java
+++ src/main/java/springCore/chapter2/ex5/Dog.java
@@ -1,11 +1,16 @@
 package springCore.chapter2.ex5;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dog {
     private String name;
 
+    @PostConstruct
+    public void init(){
+        this.name = "Kiki";
+    }
     public String getName(){
         return name;
     }
