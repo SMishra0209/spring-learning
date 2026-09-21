539dfa9903ce4ada7e799fb13fd5003026a20476
Shivesh Mishra <shiveshmishra020906@gmail.com>
2026-09-21 18:35:52 +0530

    Understanding execution chain of aspects

---
+++ src/main/java/springCore/chapter6/ex6/Main.java
@@ -0,0 +1,23 @@
+package springCore.chapter6.ex6;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static Logger logger =
+            Logger.getLogger(Main.class.getName());
+
+    static void main() {
+        var context =
+                new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var service = context.getBean(CommentService.class);
+
+        Comment comment = new Comment();
+        comment.setText("Hello World");
+        comment.setAuthor("BlaBliBlu");
+
+        String value = service.publishComment(comment);
+    }
+}
---
+++ src/main/java/springCore/chapter6/ex6/ProjectConfig.java
@@ -0,0 +1,11 @@
+package springCore.chapter6.ex6;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan
+@EnableAspectJAutoProxy
+public class ProjectConfig {
+}
